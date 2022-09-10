64a0aa1b121ab5290b13c7fd24250943cf603b30
Luan Koekemoer <luankoeke@gmail.com>
2022-09-10 20:47:50 +0200

    color changer string interpelation

--- src/flutter_app/lib/views/colour_changer.dart
+++ src/flutter_app/lib/views/colour_changer.dart
@@ -7,23 +7,23 @@ class ColourChanger extends StatefulWidget {
   State<StatefulWidget> createState() => _ColourChangerStateFull();
 }
 
-class _ColourChangerStateFull extends State<ColourChanger>{
-
+class _ColourChangerStateFull extends State<ColourChanger> {
   MaterialColor thisStateColour = Colors.green;
 
-  void _setColourAction(color){
+  void _setColourAction(color) {
     setState(() => thisStateColour = color);
   }
 
-  Widget _buildColourChangerButton(colorNameStr, color){
+  Widget _buildColourChangerButton(colorNameStr, color) {
     ButtonStyle btnStyle = ButtonStyle(
-      backgroundColor: MaterialStateProperty.resolveWith((states) => color),
-      textStyle: MaterialStateProperty.resolveWith((states) => const TextStyle(fontSize: 20))
-    );
+        backgroundColor: MaterialStateProperty.resolveWith((states) => color),
+        textStyle: MaterialStateProperty.resolveWith(
+            (states) => const TextStyle(fontSize: 20)));
 
-    String btnText = "Change to: " + colorNameStr + (color == thisStateColour ? "*" : "");
-    return ElevatedButton(onPressed: () =>
-        _setColourAction(color),
+    String btnText =
+        "Change to: ${colorNameStr} ${(color == thisStateColour ? "*" : "")}";
+    return ElevatedButton(
+        onPressed: () => _setColourAction(color),
         child: Text(btnText),
         style: btnStyle);
   }
@@ -36,22 +36,21 @@ class _ColourChangerStateFull extends State<ColourChanger>{
         backgroundColor: thisStateColour,
       ),
       body: Center(
-        child: Column(
-          children: [
-            _buildColourChangerButton("Green", Colors.green),
-            _buildColourChangerButton("Red", Colors.red),
-            _buildColourChangerButton("Blue", Colors.blue),
-            _buildColourChangerButton("Pink", Colors.pink),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go back!'),
-            ),
-          ],
-        )
-
-      ),
+          child: Column(
+        children: [
+          _buildColourChangerButton("Green", Colors.green),
+          _buildColourChangerButton("Red", Colors.red),
+          _buildColourChangerButton("Blue", Colors.blue),
+          _buildColourChangerButton("Pink", Colors.pink),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          ),
+        ],
+      )),
     );
   }
 }
+
