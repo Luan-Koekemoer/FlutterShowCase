cbdf9b383f5e05a06f4f699216f5b798f327ecf2
Luan Koekemoer <luankoeke@gmail.com>
2022-09-10 16:01:46 +0200

    resturcutred project

---
+++ src/flutter_app/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/views/colour_changer.dart';
+import 'package:flutter_app/views/scientific_calculator_api.dart';
+import 'package:flutter_app/views/paint.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  Widget _buildNavButton(context, textTo, pageToNav){
+    return ElevatedButton(onPressed: () => {Navigator.push(context, MaterialPageRoute(builder: (context) => pageToNav))},
+                          child: Text(textTo));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              _buildNavButton(context, "Colour Changer", const ColourChanger()),
+              _buildNavButton(context, "Scientific Calculator", const ScientificCalculator()),
+              _buildNavButton(context, "Paint App", const PaintApp())
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ src/flutter_app/lib/views/scientific_calculator_api.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_app/models/calc.dart';
+import 'dart:async';
+import 'dart:convert';
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+class ScientificCalculator extends StatefulWidget {
+  const ScientificCalculator({super.key});
+
+  @override
+  State<ScientificCalculator> createState() => _ScientificCalculatorState();
+}
+
+class _ScientificCalculatorState extends State<ScientificCalculator> {
+
+  /* ScientificCalculator({Key? key}) : super(key: key); */
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sci Calc'),
+      ),
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+        /* child: ElevatedButton( */
+        /*   onPressed: () { */
+        /*     Navigator.pop(context); */
+        /*   }, */
+        /*   child: const Text('Go back!'), */
+        /* ), */
+      ),
+    );
+  }
+}
+
